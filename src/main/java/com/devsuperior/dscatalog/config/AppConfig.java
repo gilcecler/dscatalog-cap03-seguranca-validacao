d0ff20582e7035663a61662dbba79ce6a5df37e5
Gilcecler Carneiro <gilcecler@gmail.com>
2024-08-13 09:26:12 -0300

    ajustes

--- src/main/java/com/devsuperior/dscatalog/config/AppConfig.java
+++ src/main/java/com/devsuperior/dscatalog/config/AppConfig.java
@@ -4,24 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class AppConfig {
 
 	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
+	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
-	@Configuration
-	public class SecurityConfig {
 
-		@Bean
-		public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-			http.csrf(csrf -> csrf.disable());
-			http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-			return http.build();
-		}
-	}
 }
